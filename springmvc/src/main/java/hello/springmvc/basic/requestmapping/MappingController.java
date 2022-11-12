85e20f57e3c5f93272981c258f59e19ef28b8095
eogus0512 <eogus0512@gmail.com>
2022-11-12 22:50:39 +0900

    스프링 MVC - 기본 기능

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,33 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingController {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping({"/hello-basic", "/hello-go"})
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    //public String mappingPath(@PathVariable("userId") String data) {
+    public String mappingPath(@PathVariable String userId){
+        log.info("mappingPath userId={}", userId);
+        return "ok";
+    }
+
+}
